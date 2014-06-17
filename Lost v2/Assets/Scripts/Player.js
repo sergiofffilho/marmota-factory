5fc3361c34d742ad77d9435a0c11c21715a0c6a2
Sergio <sergiofffilho@gmail.com>
2014-06-17 01:47:57 -0300

    Fase 1 adicionada, com movimentos para esquerda e direita

---
+++ Lost v2/Assets/Scripts/Player.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+var velocidade = 0.0;
+
+function Start () {
+
+}
+
+function Update () {
+	Movimentar();
+}
+
+function Movimentar (){
+	if(Input.GetAxisRaw("Horizontal") > 0){
+		transform.Translate(Vector2.right * velocidade * Time.deltaTime);
+		transform.eulerAngles = Vector2(0,0);
+	}
+	if(Input.GetAxisRaw("Horizontal") < 0){
+		transform.Translate(Vector2.right * velocidade * Time.deltaTime);
+		transform.eulerAngles = Vector2(0,180);
+	}
+	
+}
