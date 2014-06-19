ba59c9bb26eef29b39c4c3ca251c88d3ffb043b6
Sergio <sergiofffilho@gmail.com>
2014-06-18 21:24:29 -0300

    Pulo adicionado, animações adicionadas, background adicionado. Câmera se movimenta de acordo com o personagem.

---
+++ Lost v2/Assets/Scripts/CameraMover.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMover : MonoBehaviour {
+	
+	public Transform player;
+	public float smooth = 0;
+	public Vector2 velocidade;
+	
+	void Start () {
+		velocidade = new Vector2 (0.5f, 0.5f);
+	}
+	
+	void Update () {
+		
+		Vector2 novaPosicao = Vector2.zero;
+		
+		novaPosicao.x = Mathf.SmoothDamp (transform.position.x, player.position.x, ref velocidade.x, smooth);
+		novaPosicao.y = Mathf.SmoothDamp (transform.position.y, player.position.y, ref velocidade.y, smooth);
+		
+		Vector3 novaPosicao3d = new Vector3 (novaPosicao.x, novaPosicao.y, transform.position.z);
+		
+		transform.position = Vector3.Slerp (transform.position, novaPosicao3d, Time.time);
+	}
+}
---
+++ Lost v2/Assets/Scripts/Player.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float velocidade;
+
+	public Transform player;
+	private Animator animator;
+
+	public bool isChao;
+	public float forca = 250;
+
+	public float tempoPulo;
+	public float puloDelay = 0.5f;
+	public bool pulando;
+	public Transform chao;
+
+	void Start () {
+
+		animator = player.GetComponent<Animator> ();
+
+	}
+	
+	void Update () {
+
+		Movimentar();
+
+	}
+
+	void Movimentar(){
+
+		isChao = Physics2D.Linecast(this.transform.position, chao.position, 1<<LayerMask.NameToLayer("Frente"));
+
+		animator.SetFloat("andar", Mathf.Abs(Input.GetAxis("Horizontal")));
+
+		if (Input.GetAxisRaw ("Horizontal") > 0) {
+					transform.Translate (Vector2.right * velocidade * Time.deltaTime);
+				transform.eulerAngles = new Vector2 (0, 0);
+		}
+
+		if (Input.GetAxisRaw ("Horizontal") < 0) {
+				transform.Translate (Vector2.right * velocidade * Time.deltaTime);
+				transform.eulerAngles = new Vector2 (0, 180);
+		}
+
+		if (Input.GetButtonDown ("Jump") && isChao && !pulando) {
+			rigidbody2D.AddForce(transform.up * forca);
+			tempoPulo = puloDelay;
+			animator.SetTrigger("pular");
+			pulando = true;
+		}
+
+		tempoPulo -= Time.deltaTime;
+
+		if (tempoPulo <= 0 && isChao && pulando) {
+			animator.SetTrigger("chao");
+			pulando = false;
+		}
+
+	}
+}
