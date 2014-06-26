2126607675efa60a99b9539264a65eb3330f8597
Brendo94 <brendocolaco@hotmail.com>
2014-06-26 19:52:57 -0300

    sprite da cobra corigido, soin adicionado, mas falta lançar pedras

---
+++ Lost v2/Assets/Scripts/Pedra.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+var velocidadePedra : float;
+
+function Start () {
+velocidadePedra = 60 * Time.deltaTime;
+transform.Translate(0, 0, velocidadePedra);
+
+}
+
+function OnCollisionEnter(collision: Collision)
+{
+	Destroy(gameObject);
+
+}
---
+++ Lost v2/Assets/Scripts/Soin.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+var projetil 	: GameObject;
+
+function Start () {
+
+}
+
+function Update () {
+	Instantiate(projetil, transform.position, transform.rotation);
+}
