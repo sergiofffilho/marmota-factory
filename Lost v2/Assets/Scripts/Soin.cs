610cbcedf421dd839e2f2d8bbbe079aa3a9b6694
Brendo94 <brendocolaco@hotmail.com>
2014-06-28 23:42:08 -0300

    animação do soin jogando pedra adicionada, mas falta melhorar

--- Lost v2/Assets/Scripts/Soin.cs
+++ Lost v2/Assets/Scripts/Soin.cs
@@ -3,22 +3,31 @@ using System.Collections;
 
 public class Soin : MonoBehaviour {
 	public GameObject projetil;
+
+	public Transform soin;
+	private Animator animator;
 	// Use this for initialization
 	void Start () {
-	
+
+		animator = soin.GetComponent<Animator> ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
-	
+				
 	void OnTriggerStay2D(Collider2D other) {
 		if (other.gameObject.tag == "perdido") {
-			if(Time.time % 2 == 0){
-				Instantiate(projetil, transform.position, transform.rotation);		
-			}
 
+			if(Time.time % 2 == 0){
+				animator.SetBool("atirando2",true);
+				//animator.SetTrigger("atirando2");
+				Instantiate(projetil, transform.position, transform.rotation);		
+			
 		}
 	}
+
+}
 }
