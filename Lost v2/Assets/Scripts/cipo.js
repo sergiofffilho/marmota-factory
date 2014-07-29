d4ce8dfee8f27327ff7cf021bd10a776962aa43c
Brendo94 <brendocolaco@hotmail.com>
2014-07-29 01:41:08 -0300

    agrupados os elementos que estavam espalhados

---
+++ Lost v2/Assets/Scripts/cipo.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+var playerObject : GameObject;
+var canClimb = false;
+var speed : float = 1;
+ 
+function Start () {
+    playerObject = GameObject.FindGameObjectWithTag("perdido");
+}
+ 
+ function OnTriggerEnter (other : Collider) {
+		if(other.gameObject == playerObject){
+        canClimb = true;
+    }
+	}
+ 
+function OnTriggerExit (other : Collider) {
+		if(other.gameObject == playerObject){
+        canClimb = false;
+    }
+	}
+	
+
+function Update () {
+    if(canClimb){
+        if(Input.GetKey(KeyCode.W)){
+            playerObject.transform.Translate (Vector3(0,1,0) * Time.deltaTime*speed);
+        }
+        if(Input.GetKey(KeyCode.S)){
+            playerObject.transform.Translate (Vector3(0,-1,0) * Time.deltaTime*speed);
+        }
+    }
+}
