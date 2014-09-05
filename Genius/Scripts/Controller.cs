58484f17d91854a7ebdfabc0c1ddb5dbff8f3e6e
Sergio <sergiofffilho@gmail.com>
2014-09-05 19:08:26 -0300

    Adicionado diretório Genius.

---
+++ Genius/Scripts/Controller.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+	ArrayList sequenciaUsuario = new ArrayList();
+	ArrayList sequenciaMaquina = new ArrayList();
+	int i = 0;
+
+
+	void Start () {
+
+		GameObject.Find("Pointamarelo").SendMessage("LoopJogo");
+	}
+
+	void Update (){
+		Debug.Log (i);
+		if (i >= 10) {
+			GameObject.Find ("Pointamarelo").SendMessage ("LoopJogo");
+			GameObject.Find ("Pointverde").SendMessage ("LoopJogo");
+			GameObject.Find ("Pointazul").SendMessage ("LoopJogo");
+			GameObject.Find ("Pointvermelho").SendMessage ("LoopJogo");
+		}
+		i++;
+
+
+	}
+
+	IEnumerator gerarSequencia(){
+		int n = Random.Range (1, 5);
+		Debug.Log("AQUI");
+		if(n==1){
+			sequenciaMaquina.Add("verde");
+			for (int i=0; i<sequenciaMaquina.Count; i++) {
+				GameObject.Find("Point"+sequenciaMaquina[i]).SendMessage("receberTagMaquina", sequenciaMaquina[i]);
+				yield return new WaitForSeconds(1.5f);
+			}
+
+		}
+		else if (n==2){
+			sequenciaMaquina.Add("vermelho");
+			for (int i=0; i<sequenciaMaquina.Count; i++) {
+				GameObject.Find("Point"+sequenciaMaquina[i]).SendMessage("receberTagMaquina", sequenciaMaquina[i]);
+				yield return new WaitForSeconds(1.5f);
+			}
+		}
+		else if (n==3){
+			sequenciaMaquina.Add("azul");
+			for (int i=0; i<sequenciaMaquina.Count; i++) {
+				GameObject.Find("Point"+sequenciaMaquina[i]).SendMessage("receberTagMaquina", sequenciaMaquina[i]);
+				yield return new WaitForSeconds(1.5f);
+			}
+		}
+		else if (n==4){
+			sequenciaMaquina.Add("amarelo");
+			for (int i=0; i<sequenciaMaquina.Count; i++) {
+				GameObject.Find("Point"+sequenciaMaquina[i]).SendMessage("receberTagMaquina", sequenciaMaquina[i]);
+				yield return new WaitForSeconds(1.5f);
+			}
+		}
+
+	}
+
+	/*void compararSequencias(){
+		for (int i=0; i<sequenciaUsuario.Count; i++) {
+			if(sequenciaUsuario[1].Equals(sequenciaMaquina[1])){
+				Debug.Log("acertou");
+			} else{
+				Debug.Log("errou");
+			}
+		}
+	}*/
+
+	void print () {
+		if (Input.GetMouseButtonDown (0)) {				
+			foreach (string tag in sequenciaUsuario) {
+				Debug.Log (tag+" usu");
+			}
+			foreach (string tag in sequenciaMaquina) {
+				Debug.Log (tag+" maq");
+			}
+		}
+	}
+
+	void adicionarTag(string tag){
+		sequenciaUsuario.Add (tag);
+	}
+
+	IEnumerator tempoEspera(){
+		yield return new WaitForSeconds (1.5f);		
+	}
+
+
+}
---
+++ Genius/Scripts/Genius.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Genius : MonoBehaviour {
+
+	//Variavel que verifica a area de colisao com cada cubo.
+	public bool ascender;
+	bool maquina = true;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void Update(){
+
+		}
+
+	// Update is called once per frame
+	void LoopJogo () {
+
+		if (maquina) {
+			maquina = false;
+			GameObject.Find ("Pai").SendMessage ("gerarSequencia");
+
+		} else {
+
+			if (ascender) {
+
+				if (Input.GetMouseButtonDown (0)) {	
+					GameObject.Find ("Pai").SendMessage ("adicionarTag", gameObject.tag);
+					light.intensity = 1;										
+					GameObject.Find ("Pai").SendMessage ("print");
+					//GameObject.Find("Pai").SendMessage("compararSequencias");
+					audio.Play();
+					maquina = true;
+					StartCoroutine(tempoEspera());						
+				}
+			}
+		}
+	}
+
+	void receberTagMaquina(string tag){
+		light.intensity = 1;
+		audio.Play();
+		StartCoroutine (tempoEspera ());
+	}
+
+	public void OnMouseEnter(){
+		ascender = true;
+		
+	}
+	
+	void OnMouseExit(){
+		ascender = false;
+	}
+
+	IEnumerator tempoEspera(){
+		yield return new WaitForSeconds (1f);
+		light.intensity = 0;
+
+	}
+	
+}
